e2d818ca0de3f0ff32010abf62bfddee9488ba13
Shyam <patrick.shyamkumar@gmail.com>
2019-12-09 11:02:35 +0800

    Update gitignore

--- backend/routes/auth.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const UserController = require('../controllers/auth');
-
-router.post('/signup', UserController.createUser);
-
-router.post('/login', UserController.userLogin);
-
-module.exports = router;
